98ecccc0f7e9f5f7203d1747b880cab5259dc5f1
ShaikhAshfaque <sh.ashfaqueme4049@gmail.com>
2026-09-25 18:34:36 +0530

    Update Super 30 project

---
+++ introduction.py
@@ -0,0 +1,22 @@
+# string - String is generally used for storing text data and can be used single and double quotes '' & "".
+
+name = "Shaikh Ashfaque"
+city = "Delhi"
+profession = "Data Scientist"
+experience = "7 Years"
+education = "B.tech"
+skill = "Python"
+goal = "I want to FDE"
+interest = " FDE"
+hobby = "Learnin new technogoly"
+
+print(name)
+print(city)
+print(profession)
+print(experience)
+print(education)
+print(skill)
+print(goal)
+print(interest)
+print(hobby)
+ 
