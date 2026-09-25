37ff4cd63e56120e85dda1fd5a30fbba814f9266
ShaikhAshfaque <sh.ashfaqueme4049@gmail.com>
2026-09-25 23:58:05 +0530

    list tuple queues file

---
+++ list_tuples_queues.py
@@ -0,0 +1,46 @@
+# List #tuples & Queues
+
+# List - Remember points
+# List is used square bracked 
+# List is mutable 
+# Methods : support append(), extend(),inset(), remove(),sort() etc
+
+
+# List 
+#List 1 - 
+skills = ["Python","Git","AI","SQL"]
+
+#List 2 -
+courses = ["Python","Data Science","GenAI"]
+
+#List 3 -
+hobbies = ["Reading","Writing","Learning","Travelling"]
+
+
+# Tuples
+# Tuples is also storing data but into the square bracket
+# Tuple is immutable means you can't change the value once created
+# Method supporting : Mainly count(), index()
+
+#Tuple -1 
+location = ("Delhi","Noida","Gurgaon")
+
+#Tuple - 2
+company = ("karix","Bangalore","India")
+
+#Tuple - 3
+date_of_birth = (24, "October", 1996)
+
+
+#Queues - Tasks
+# FIFO - FIrst in First out 
+
+#Queus - 1
+support_queues = ["Ticket 101", "Ticket 102", "Ticket 103"]
+
+#Queues - 2
+customer_queues = ["customer1", "customer2","customer3"]
+
+#Queus - 3
+task_queues = ["Task 1","Task 2","Task 3"]
+
