fc7b008e1232a0bfce27f2c6a3528cede2972554
ShaikhAshfaque <sh.ashfaqueme4049@gmail.com>
2026-09-25 17:56:09 +0530

    add introduction file

---
+++ indtroduction.py
@@ -0,0 +1,23 @@
+# 2.String and Personal Induction Program
+
+# string - string is normally used to store texts data and the single and double quotes can be used '' & "".add()
+
+name = "Shaikh Ashaque"
+city = "Delhi"
+profession = 'Data Scientist'
+experience = "7 Years experience"
+skills = "Python"
+education = "B.tech"
+interest = "Forward Deployment engineer"
+goal = "I want to become FDE"
+hobby = "Learning new techology"
+
+print(name)
+print(city)
+print(profession)
+print(experience)
+print(skills)
+print(education)
+print(interest)
+print(goal)
+print(hobby)
